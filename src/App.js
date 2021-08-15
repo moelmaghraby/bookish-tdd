e2471fd72fa1c2c56451b84b68be4e607f418dda
Mohamed Osman <mohamed.o@scopicsoftware.com>
2021-08-15 22:21:08 +0200

    DEV: setup routing and adding routes for booklist and book details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Typography } from '@material-ui/core';
+import BookListContainer from './BooklistContainer';
+import { Route } from 'react-router';
+import BookDetailContainer from './BookDetailsContainer';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Typography variant="h2" component="h2" data-test="heading"> Bookish</Typography>
+      <Route path="/" component={BookListContainer} exact={true}></Route>
+      <Route path="/books/:id" component={BookDetailContainer}></Route>
     </div>
   );
 }
